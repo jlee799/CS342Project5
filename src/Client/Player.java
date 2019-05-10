59f2fa5f046c1b88f080547531065be7be1e114a
allenchung01 <allenchung01@gmail.com>
2019-05-09 21:33:14 -0500

    GUI added to ClientProcessing.java but not implemented yet

---
+++ src/Client/Player.java
@@ -0,0 +1,147 @@
+package Client;
+
+public class Player {
+	
+	private String username;
+	private String id;
+	private int roll;
+	
+	private String weapon;
+	private String helmet;
+	private String chest;
+	private String legs;
+	private String item;
+	
+	private int health;
+	private int atk;
+	private int def;
+	
+	private String move;
+	private boolean defending;
+	private boolean usedItem;
+	
+	public Player(){
+		username = null;
+		id = null;
+		roll = 0;
+		
+		weapon = null;
+		helmet = null;
+		chest = null;
+		legs = null;
+		item = null;
+		
+		health = 100;
+		atk = 0;
+		def = 0;
+		
+		move = null;
+		defending = false;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void setHealth( int hp) {
+		health = hp;
+	}
+	
+	public int getRoll() {
+		return roll;
+	}
+	
+	public void setRoll( int val) {
+		roll = val;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId( String val) {
+		id = val;
+	}
+	
+	public void setUsername( String name) {
+		username = name;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getWeapon() {
+		return weapon;
+	}
+	
+	public void setWeapon( String weap) {
+		weapon = weap;
+	}
+	
+	public String getHelmet() {
+		return helmet;
+	}
+	
+	public void setHelmet( String helm) {
+		helmet = helm;
+	}
+	
+	public String getChest() {
+		return chest;
+	}
+	
+	public void setChest( String ches) {
+		chest = ches;
+	}
+	
+	public String getLegs() {
+		return legs;
+	}
+	
+	public void setLegs( String leg) {
+		legs = leg;
+	}
+	
+	public String getItem() {
+		return item;
+	}
+	
+	public void setItem( String leg) {
+		legs = leg;
+	}
+	
+	public void setAtk( int attack) {
+		atk = attack;
+	}
+	
+	public int getAtk() {
+		return atk;
+	}
+	
+	public void setDef( int defense) {
+		def = defense;
+	}
+	
+	public int getDef() {
+		return def;
+	}
+	
+	public String getMove() {
+		return move;
+	}
+	
+	public void setMove( String moveString) {
+		move = moveString;
+	}
+	
+	public void setDefending( boolean bool) {
+		defending = bool;
+	}
+	
+	public boolean getDefending() {
+		return defending;
+	}
+	
+
+}
