68fb0cfbca601a11a0c4e05506f3dcdb37295407
seanennis14 <49453772+seanennis14@users.noreply.github.com>
2019-05-09 19:12:38 -0500

    Add files via upload

--- src/ClientProcessing.java
+++ src/ClientProcessing.java
@@ -7,10 +7,12 @@ public class ClientProcessing {
 	
 	String ipAdd;
 	int portNum;
-	String idNum;
+	String idNumStr;
+	int idNumInt;
 	private ClientNetworkConnectionRPS conn = null;
 	String username;
-	ArrayList<Player> players = new ArrayList<Player>();
+	ArrayList<Player> players = new ArrayList<Player>(4);
+	//StateOfGame game;
 	
 	private ClientRPS createClient() {
         
@@ -32,38 +34,103 @@ public class ClientProcessing {
         switch(tokens[0]) {
         
         	case "id":
-        		idNum = tokens[1];
+        		setID(tokens[1]);
+        		//idNum = tokens[1];
         	case "roll":
-        		
+        		setRoll(tokens[1], tokens[2]);
         	case "choose":
         		
         	case "picked":
-        		
+        		setPick(tokens[1], tokens[2], tokens[3]);
         	case "start":
-        		
-        	case "attack":
-        		
-        	case "defend":
-        		
-        	case "item":
-        		
+        		start();
+        	case "atkStat":
+        		setAtkStat(tokens[1],tokens[2]);
+        	case "defStat":
+        		setDefStat(tokens[1],tokens[2]);
         	case "health":
         		updateHealth(tokens[1], tokens[2]);
         	case "nextRound":
         		startNextRound();
+        	case "winner":
+        		winner(tokens[1]);
+        	case "text":
+        		receiveMessage(tokens[1]);
         }
         
+	}
+	
+	public void setID(String id) {
+		idNumStr = id;
+		idNumInt = Integer.parseInt(id);
+	}
+	
+	public void setRoll(String id, String numRolledStr) {
+		int idInt = Integer.parseInt(id);
+		int numRolled = Integer.parseInt(numRolledStr);
+		players.get(idInt-1).setRoll(numRolled);
+	}
+	
+	public void choose() {
+		
+	}
+	
+	public void setPick(String id, String pickType, String pick) {
+		int idInt = Integer.parseInt(id);
+		
+		switch(pickType) {
+			case "sword":
+				players.get(idInt-1).setWeapon(pick);
+			case "helmet":
+				players.get(idInt-1).setHelmet(pick);
+			case "chest":
+				players.get(idInt-1).setChest(pick);
+			case "legs":
+				players.get(idInt-1).setLegs(pick);
+			case "item":
+				players.get(idInt-1).setItem(pick);
+		}
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void setAtkStat(String id, String atkStat) {
+		int atkInt = Integer.parseInt(atkStat);
+		int idInt = Integer.parseInt(id);
+		players.get(idInt-1).setAtk(atkInt);
+	}
+	
+	public void setDefStat(String id, String defStat) {
+		int defInt = Integer.parseInt(defStat);
+		int idInt = Integer.parseInt(id);
+		players.get(idInt-1).setDef(defInt);
 	}
 	
 	public void updateHealth(String id, String health) {
 		int idInt = Integer.parseInt(id);
 		int healthInt = Integer.parseInt(health);
-		players[idInt].setHealth = Integer.parseInt(health);
+		players.get(idInt-1).setHealth(healthInt);
 	}
 	
 	public void startNextRound() {
 		
 	}
+	
+	public void winner(String winnerID) {
+		if(winnerID.equals("0")) {
+			//No winner - everyone died
+		}
+		else {
+			//Display winner
+		}
+	}
+	
+	public void receiveMessage(String message) {
+		
+	}
+
 	
 	
 	//Sending To Client
@@ -72,7 +139,7 @@ public class ClientProcessing {
 		username = "";
 		//Get username from GUI
 		try {
-			conn.send("connect-" + idNum + "-" + username);
+			conn.send("connect-" + idNumStr + "-" + username);
 		}
 		catch(Exception e){
 			System.out.println(e.getMessage());
@@ -84,16 +151,17 @@ public class ClientProcessing {
 		String numRolled = Integer.toString(random.nextInt(6) + 1);
 		
 		try {
-			conn.send("roll-" + idNum + "-" + numRolled);
+			conn.send("roll-" + idNumStr + "-" + numRolled);
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	public void pickItem(String itemStr) {
+	public void pickItem(String pickType, String pick) {
+		setPick(idNumStr, pickType, pick);
 		try {
-			conn.send("pick-" + idNum + "-" + itemStr);
+			conn.send("pick-" + idNumStr + "-" + pickType + "-" + pick);
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
@@ -105,7 +173,7 @@ public class ClientProcessing {
 		//Get player to attack here from GUI or as parameter
 		
 		try {
-			conn.send("attack-" + idNum + "-" + playerToAttack);
+			conn.send("attack-" + idNumStr + "-" + playerToAttack);
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
@@ -114,7 +182,7 @@ public class ClientProcessing {
 	
 	public void defend() {
 		try {
-			conn.send("defend-" + idNum);
+			conn.send("defend-" + idNumStr);
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
@@ -126,7 +194,7 @@ public class ClientProcessing {
 		//Get item here from GUI or as parameter
 		
 		try {
-			conn.send("item-" + idNum + "-" + itemUsed);
+			conn.send("item-" + idNumStr + "-" + itemUsed);
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
